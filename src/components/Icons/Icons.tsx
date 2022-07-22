ebb5cac7a4ad8121507bb733567688f2064c4aa9
Alexandr Yanykin <numelen@gmail.com>
2022-07-22 23:25:27 +0300

    feat: added icons

---
+++ src/components/Icons/Icons.tsx
@@ -0,0 +1,43 @@
+import { SVGAttributes, FC } from 'react';
+
+interface IconProps extends SVGAttributes<SVGElement> {
+  size?: number;
+}
+
+const withDefaultProps =
+  (Icon: FC<IconProps>) =>
+  ({ size = 16, color = 'currentColor', ...props }: IconProps) =>
+    <Icon size={size} color={color} {...props} />;
+
+const Trash = withDefaultProps(({ size, color }: IconProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    fill={color}
+    viewBox="0 0 16 16"
+  >
+    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+    <path
+      fill-rule="evenodd"
+      d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+    />
+  </svg>
+));
+
+const Exclamation = withDefaultProps(({ size, color }: IconProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    fill={color}
+    viewBox="0 0 16 16"
+  >
+    <path d="M7.005 3.1a1 1 0 1 1 1.99 0l-.388 6.35a.61.61 0 0 1-1.214 0L7.005 3.1ZM7 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0Z" />
+  </svg>
+));
+
+export const Icons = {
+  Trash,
+  Exclamation,
+};
